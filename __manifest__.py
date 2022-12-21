d3e098c71e62a2925fd683cc7be66104622c643a
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2022-12-21 13:05:44 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Payment Exchange Rate',
+    'version': '14.0.1',
+    'category': 'Accounting',
+    'depends': [
+        'account'
+        ],
+    'data':[
+        'payment_view.xml'
+    ],
+    'installable' : True,
+    'application' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
